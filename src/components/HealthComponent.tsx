f59c188aaf5fcd132aa808ec452b134300762f47
Albert Attard <albert.attard@oracle.com>
2023-07-28 22:37:17 +0200

    Fetch status from backend

--- src/components/HealthComponent.tsx
+++ src/components/HealthComponent.tsx
@@ -1,12 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './HealthComponent.css';
 
-export const HealthComponent = () => {
-  const [health, setHealth] = useState("...")
-  const checkHealth = () => { setHealth("up") }
+type HealthResponse = {
+  status: string;
+}
 
-  return <>
-    <p className='health-status'>Backend status {health}</p>
-    <button onClick={checkHealth}>Check now</button>
-  </>
+export const HealthComponent = () => {
+  const [health, setHealth] = useState<HealthResponse>({ status: '...' });
+  const [style, setStyle] = useState<string>('checking')
+
+  useEffect(() => {
+    const checkHealth = async () => {
+      fetch('/api/actuator/health')
+        .then(response => response.json())
+        .then(data => {
+          setHealth(data);
+          setStyle(data.status === 'UP' ? 'up' : 'unreachable');
+        })
+        .catch(e => {
+          setHealth({ status: 'Unreachable' });
+          setStyle('unreachable');
+        })
+    };
+
+    checkHealth();
+  }, []);
+
+  return (<>
+    <p className={style}>Backend status {health.status}</p>
+  </>);
 }
